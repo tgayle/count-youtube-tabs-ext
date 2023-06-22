6d578d42370d5d872b48cfcce08c83a708658c70
tgayle <travisgayledev@gmail.com>
2023-06-21 21:44:06 -0700

    Pass video/videoInfo directly to VideoItem

--- src/components/VideoItem.tsx
+++ src/components/VideoItem.tsx
@@ -1,39 +1,36 @@
 import { Show } from "solid-js";
-import { VideoId, VideoWithInfo } from "../api";
+import { VideoWithInfo } from "../api";
 import { secondsToDuration } from "../util";
 
 type Props = {
   videoId: string;
-  tabs: Record<VideoId, chrome.tabs.Tab>;
-  videoInfo: Record<VideoId, VideoWithInfo>;
+  tab: chrome.tabs.Tab;
+  info?: VideoWithInfo;
   progress: number;
 };
 
 export function VideoItem(props: Props) {
-  const tab = () => props.tabs[props.videoId];
-  const info = () => props.videoInfo[props.videoId];
-
   return (
     <li class="p-4 shadow-md m-2 flex justify-between">
       <div class="pr-1 flex flex-col justify-between grow">
-        <p>{tab().title}</p>
+        <p class="mt-2">{props.tab.title}</p>
 
         <Show when={props.progress > 0}>
           <div
             class="tooltip w-full"
             data-tip={`${secondsToDuration(props.progress)}/${secondsToDuration(
-              info()?.duration ?? 0
+              props.info?.duration ?? 0
             )}`}
           >
             <progress
               class="progress my-2 pr-2 grow"
               value={props.progress}
-              max={info()?.duration ?? 0}
+              max={props.info?.duration ?? 0}
             ></progress>
           </div>
         </Show>
       </div>
-      <Show when={info()}>
+      <Show when={props.info}>
         {(info) => (
           <div class="flex flex-col gap-2">
             <span class="badge badge-primary">
@@ -42,8 +39,8 @@ export function VideoItem(props: Props) {
             <button
               class="btn btn-sm btn-outline"
               onClick={async () => {
-                chrome.tabs.update(tab().id!, { active: true });
-                chrome.windows.update(tab().windowId!, { focused: true });
+                chrome.tabs.update(props.tab.id!, { active: true });
+                chrome.windows.update(props.tab.windowId!, { focused: true });
               }}
             >
               Go
